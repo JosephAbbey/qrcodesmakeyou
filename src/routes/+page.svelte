8c28e4df49664343ce88dd465d4657092dfae9bc
Joseph Abbey <me@josephabbey.dev>
2023-06-23 18:31:51 +0100

    update default url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
   import { notify } from '../stores/notifications';
   export let data: PageData;
 
-  let url = $page.url.searchParams.get('shared_url') ?? 'https://josephabbey.dev/';
+  let url = $page.url.searchParams.get('shared_url') ?? 'https://qr.josephabbey.dev/';
   let theme: Theme | undefined = undefined;
   let code: QRCode;
 
